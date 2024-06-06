70a310ec4b4ace111bf9c8a4703eb1119682b9be
Takenoko3333 <153407565+Takenoko3333@users.noreply.github.com>
2024-06-06 14:33:21 +0900

    Add files via upload

---
+++ README.md
@@ -0,0 +1,77 @@
+# sd-webui-xnview-pnginfo
+
+[日本語](#日本語) | [English](#english)
+
+# 日本語
+
+# 説明
+
+- Hires.fix 有効化時に Reuse seed と ADtailer を同時に有効化します。
+- Hires.fix ボタンの上に Hires.fix with ♻️ & ADtailer のチェックボックスが追加されます。
+- Hires.fix with ♻️ & ADtailer のチェックボックスがオンの場合、Hires.fix をオンにした時に Reuse seed と ADtailer を同時に有効化します。Hires.fix をオフにした時は Random seed を有効化し ADtailer をオフにします。
+- ADtailer がインストールされていない場合は ADtailer の操作は無視され、Reuse seed(Random seed)のみが連動します。
+
+  <br><br>
+  ![image](images/image.png)
+  <br><br>
+
+# インストール方法
+
+AUTOMATIC1111 の Extensions タブをクリック、Install from URL タブをクリックし、URL for extension's git repository に以下の url を入力し Install ボタンをクリックしてください。
+
+```
+https://github.com/Takenoko3333/sd-webui-reuse-seed-plus.git
+```
+
+<br>
+
+# 変更履歴
+
+## [0.1.0] - 2024-6-6
+
+### 追加
+
+- v0.1.0 リリース
+  <br><br>
+
+# ライセンス
+
+Copyright © 2024 Takenoko  
+Released under the [MIT License](https://opensource.org/licenses/mit-license.php).
+<br><br><br>
+
+# English
+
+# Description
+
+- When Hires.fix is enabled, both Reuse seed and ADtailer are enabled simultaneously.
+- A checkbox labeled Hires.fix with ♻️ & ADtailer is added above the Hires.fix button.
+- When the Hires.fix with ♻️ & ADtailer checkbox is on, enabling Hires.fix also enables both Reuse seed and ADtailer. When Hires.fix is turned off, Random seed is enabled and ADtailer is turned off.
+- If ADtailer is not installed, the ADtailer operation is ignored, and only Reuse seed (Random seed) will be toggled accordingly.
+
+  <br><br>
+  ![image](images/image.png)
+  <br><br>
+
+# Installation method
+
+Click the Extensions tab in AUTOMATIC1111, click the Install from URL tab, enter the following URL in the URL for extension's git repository field, and click the Install button.
+
+```
+https://github.com/Takenoko3333/sd-webui-reuse-seed-plus.git
+```
+
+<br>
+
+# Changelog
+
+## [0.1.0] - 2024-6-6
+
+### Added
+
+- v0.1.0 release
+
+# License
+
+Copyright © 2024 Takenoko  
+Released under the [MIT License](https://opensource.org/licenses/mit-license.php).
---
+++ javascript/reuse-seed-plus.js
@@ -0,0 +1,72 @@
+function handleHiresFixChange() {
+  const hiresFixCheckbox = document.getElementById(
+    "txt2img_hr-visible-checkbox"
+  );
+  const dynamicCheckbox = document.getElementById("reuse_seed_plus_checkbox");
+  const targetCheckbox1 = document.getElementById(
+    "script_txt2img_adetailer_ad_enable input"
+  );
+  const targetButton1 = document.getElementById("txt2img_random_seed");
+  const targetButton2 = document.getElementById("txt2img_reuse_seed");
+
+  if (dynamicCheckbox.checked) {
+    if (hiresFixCheckbox.checked) {
+      if (targetCheckbox1) {
+        targetCheckbox1.checked = true;
+        targetCheckbox1.dispatchEvent(new Event("change", { bubbles: true }));
+      }
+      if (targetButton2) targetButton2.click();
+    } else {
+      if (targetCheckbox1) {
+        targetCheckbox1.checked = false;
+        targetCheckbox1.dispatchEvent(new Event("change", { bubbles: true }));
+      }
+      if (targetButton1) targetButton1.click();
+    }
+  }
+}
+
+function createReuseSeedPlusArea() {
+  const reuseSeedPlus = document.getElementById("reuse_seed_plus");
+  const hiresFixCheckbox = document.getElementById(
+    "txt2img_hr-visible-checkbox"
+  );
+  const settings = document.getElementById("txt2img_settings");
+  const accordions = document.getElementById("txt2img_accordions");
+
+  if (!accordions || reuseSeedPlus) return;
+
+  const reuseSeedPlusArea = document.createElement("div");
+  reuseSeedPlusArea.id = "reuse_seed_plus";
+  reuseSeedPlusArea.style = "padding: 0.2em 0em";
+
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.id = "reuse_seed_plus_checkbox";
+  checkbox.className = "svelte-1ojmf70";
+
+  const label = document.createElement("label");
+  label.id = "reuse_seed_plus_label";
+  label.className = "svelte-1ojmf70";
+  label.style = "margin-left: 0.4em";
+  label.setAttribute(
+    "title",
+    "Enable reuse seed and ADtailer at the same time when enabling Hires.fix"
+  );
+
+  const span = document.createElement("span");
+  span.className = "ml-2 svelte-1ojmf70";
+  span.textContent = "Hires.fix with ♻️ & ADtailer";
+
+  label.appendChild(checkbox);
+  label.appendChild(span);
+  reuseSeedPlusArea.appendChild(label);
+
+  if (accordions) {
+    settings.insertBefore(reuseSeedPlusArea, accordions);
+  }
+
+  hiresFixCheckbox.addEventListener("change", handleHiresFixChange);
+}
+
+onUiUpdate(createReuseSeedPlusArea);
